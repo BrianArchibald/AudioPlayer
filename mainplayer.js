edbf3a2ae1acdecd1311be2709194650428b73e5
Brian Archibald <brianallenarchibald@gmail.com>
2018-06-02 19:36:21 -0500

    more js

--- mainplayer.js
+++ mainplayer.js
@@ -13,4 +13,85 @@ function playSong() {
 	songTitle.textContent = songs[currentSong];
 	song.play(); // plays the song
 }
- 
+
+// function playOrPauseSong() {
+// 	if(song.paused) {
+// 		song.play();
+// 		$('#play img').attr('src','Pause.png');
+// 	} else {
+// 		song.pause();
+// 		$('#play img').attr('src','Play.png');
+// 	}
+// }
+
+
+
+song.addEventListener('timeupdate', function() {
+	var position = song.currentTime / song.duration;
+	fillTime.style.width = `${position * 100}%`;
+
+	convertTime(Math.round(song.currentTime));
+
+	if(song.ended) {
+		next();
+	}
+});
+
+fillTime.addEventListener('click', seekTime);
+
+function seekTime() {
+	document.getElementById('fillTime').innerHTML = song.currentTime;
+	var percent = e.offsetX / this.offsetWidth;
+    song.currentTime = percent * song.duration;
+    fillTime.value = percent * 100;
+}
+
+function convertTime(seconds) {
+	var min = Math.floor(seconds / 60);
+	var sec = seconds % 60;
+
+	min = (min < 10) ? '0' + min : min;
+	sec = (sec < 10) ? '0' + sec : sec;
+	currentTime.textContent = min + ':' + sec;
+
+	totalTime(Math.round(song.duration));
+}
+
+function totalTime(seconds) {
+	var min = Math.floor(seconds / 60);
+	var sec = seconds % 60;
+
+	min = (min < 10) ? '0' + min : min;
+	sec = (sec < 10) ? '0' + sec : sec;
+	currentTime.textContent += '/' + min + ':' + sec;
+}
+
+// function next() {
+// 	currentSong++;
+// 	if(currentSong > 2) {
+// 		currentSong = 0;
+// 	}
+// 	playSong();
+// 	$('#play img').attr('src',"Pause.png");
+// 	$('#image img').attr('src',poster[currentSong]);
+// 	$('#bg img').attr('src',poster[currentSong]);
+// }
+
+// function pre() {
+// 	currentSong--;
+// 	if(currentSong < 0) {
+// 		currentSong = 2;
+// 	}
+// 	playSong();
+// 	$('#play img').attr('src',"Pause.png");
+// 	$('#image img').attr('src',poster[currentSong]);
+// 	$('#bg img').attr('src',poster[currentSong]);
+// }
+
+function decreaseVolume() {
+	song.volume > 0 && (song.volume = parseFloat(song.volume.toFixed(2)) - .2);
+}
+
+function increaseVolume() {
+	song.volume !== 1 && (song.volume += 0.20);
+}
